2236d39c474c40383f0c5e0e15cc931b15f4b8c7
GokulPrasath <74815214+Gokulprasathk@users.noreply.github.com>
2024-03-09 16:27:08 +0530

    Delete src/main/webapp/app directory

--- src/main/webapp/app/controllers/application.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Controller.extend({
-    currentUser: Ember.inject.service(),
-    isAuthenticate: Ember.computed('currentUser.isAuthenticated', function () {
-        return this.get('currentUser').get('isAuthenticated');
-    })
-});
--- src/main/webapp/app/helpers/check-user.js
+++
@@ -1,7 +0,0 @@
-import { helper } from '@ember/component/helper';
-
-export function checkUser(userRole) {
-  if(userRole==1) return true;
-}
-
-export default helper(chunkArray);
